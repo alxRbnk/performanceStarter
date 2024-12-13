40bbdd4e8dd9699793b698777bf8dcb75aef463b
Alexey Rubnikovich <liftedfish@gmail.com>
2024-12-13 17:11:11 +0100

    init

---
+++ src/main/java/com/rbnk/performance/ApplicationMonitor.java
@@ -0,0 +1,13 @@
+package com.rbnk.performance;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMonitor {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMonitor.class);
+
+    }
+}
